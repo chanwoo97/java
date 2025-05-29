98c2ba3fc13e6e9ed1b5d6de82d46125a50f4777
chanwoo97 <jcw6223@naver.com>
2025-05-29 21:58:16 +0900

    0529 미니실습 과제

---
+++ ch02/Minitest.java
@@ -0,0 +1,17 @@
+public class Minitest {
+    public static void main(String[] args){
+        String s;
+        String n;
+        String e;
+    
+        s = "01034456223";
+        n = "정찬우";
+        e = "jcw6223@naver.com";
+        
+        System.out.println("저는 : " + n + "입니다.");
+        System.out.println("제 전화번호 : " + s + "이고,");
+        System.out.println("이메일 : " + e + "입니다.");
+
+    }
+    
+}
